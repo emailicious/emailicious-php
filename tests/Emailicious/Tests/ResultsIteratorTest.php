5c752474e259c2923c3c0837f0657e56c647d72e
Simon Charette <simon.charette@emailicious.com>
2015-05-11 16:35:17 -0400

    Fixed a syntax error in PHP 5.3

--- tests/Emailicious/Tests/ResultsIteratorTest.php
+++ tests/Emailicious/Tests/ResultsIteratorTest.php
@@ -7,9 +7,10 @@ use Emailicious\Tests\EmailiciousTest;
 
 class ResultsIteratorTest extends EmailiciousTest {
 	public function testIteration() {
-		$this->client->shouldReceive('get')->with('results?page=2')->once()->andReturn(
-			array('results' => ['3', '4', '5'], 'next' => NULL)
-		);
+		$this->client->shouldReceive('get')->with('results?page=2')->once()->andReturn(array(
+			'results' => array('3', '4', '5'),
+			'next' => NULL,
+		));
 		$response = array(
 			'next' => 'https://foo.emailicious.com/api/v1/results?page=2',
 			'results' => array('0', '1', '2')
